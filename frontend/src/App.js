a362fffa924afcb9e1dd6f6be810748b05530489
Simran Nayak <simrannayak763@gmail.com>
2025-12-11 20:48:04 +0530

    Test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import TestTailwind from './components/TestTailwind';
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>AI Resume Analyzer 🚀</h1>
+      <h1>AI Resume Analyzer </h1>
       <p>Upload your resume to get insights & ATS optimization suggestions.</p>
       <UploadResume />
       <TestTailwind />;
